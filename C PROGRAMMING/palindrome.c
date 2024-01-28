06514a3bafe7c833c819ace20dd6d0e33a080852
MohamaduRiyas <144114879+MohamaduRiyas@users.noreply.github.com>
2024-01-28 13:18:24 +0530

    Add files via upload
    Enter a string:riyas
    Not a palindrome
    Enter a string:csc
    Palindrome

---
+++ C PROGRAMMING/palindrome.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int len, i, count = 0;
+
+    printf("Enter a string:");
+    char a[10];
+    scanf("%s", &a);
+
+    len = strlen(a);
+    for(i = 0; i < len; ++i){
+        if(a[i] == a[len - i -1])
+            count ++;
+    }
+    if(len == count)
+        printf("Palindrome");
+    else
+        printf("Not a palindrome");
+}
